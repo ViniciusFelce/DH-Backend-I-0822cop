c5ed4b5e5a1d295450704e97f70558d30b60b4e1
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2022-08-14 08:02:04 -0300

    Aula04 - Exemplo do DP Chain of Responsibility.

--- 04 - Chain of Responsibility pattern/Aula04/src/entities/GerenciadorGerencia.java
+++ 04 - Chain of Responsibility pattern/Aula04/src/entities/GerenciadorGerencia.java
@@ -5,7 +5,7 @@ public class GerenciadorGerencia extends Gerenciador {
     @Override
     public void verificar(Mail email) {
         if ((email.getDestino().equalsIgnoreCase("gerencia@colmeia.com")) || (email.getAssunto().equalsIgnoreCase("Gerência"))) {
-            System.out.println("E-mail enviado para o departamento Gerencial.");
+            System.out.println("E-mail enviado para o departamento Gerencial da empresa.");
         } else {
             if (this.getGerenciadorSeguinte() != null) {
                 this.getGerenciadorSeguinte().verificar(email);
