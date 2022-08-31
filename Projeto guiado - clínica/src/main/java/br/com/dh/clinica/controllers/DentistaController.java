5abdf535f387c5984abe6883cf7c24d565dc3d71
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2022-08-31 10:24:30 -0300

    Projeto guiado - Clínica Odontológica.

--- Projeto guiado - clínica/src/main/java/br/com/dh/clinica/controllers/DentistaController.java
+++ Projeto guiado - clínica/src/main/java/br/com/dh/clinica/controllers/DentistaController.java
@@ -5,11 +5,10 @@ import br.com.dh.clinica.entities.Dentista;
 import br.com.dh.clinica.services.DentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,6 +28,20 @@ public class DentistaController {
     public ResponseEntity<DentistaDto> buscarDentistaPorId(@PathVariable Integer id) {
         DentistaDto dto = service.buscarPorId(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> excluirDentista(@PathVariable Integer id) {
+        service.excluir(id);
+        return ResponseEntity.noContent().build(); // Retorna 204
+    }
+
+    @PostMapping
+    public ResponseEntity<DentistaDto> inserirDentista(@RequestBody DentistaDto dto) {
+        dto = service.inserir(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
