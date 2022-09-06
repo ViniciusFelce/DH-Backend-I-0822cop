a8447b25a5e4f4ae4479adffbce6cdbe08a64328
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2022-09-06 09:58:06 -0300

    Aula31 - ORM - @OneToMany e @ManyToOne - Aluno/Professor.

---
+++ 31 - ORM/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO professor (nome, curso) VALUES ('Marcelo', 'CTD');
+INSERT INTO professor (nome, curso) VALUES ('Christh', 'CTD');
+INSERT INTO aluno (nome, idade, professor_id) VALUES ('Elaine', 27, 1);
+INSERT INTO aluno (nome, idade, professor_id) VALUES ('Vinicius', 25, 1);
+INSERT INTO aluno (nome, idade, professor_id) VALUES ('Rodrigo', 29, 2);
+INSERT INTO aluno (nome, idade, professor_id) VALUES ('Júlia', 21, 2);
+INSERT INTO aluno (nome, idade, professor_id) VALUES ('Jailson', 23, 1);
--- Projeto guiado - clínica/src/main/java/br/com/dh/clinica/entities/Dentista.java
+++ Projeto guiado - clínica/src/main/java/br/com/dh/clinica/entities/Dentista.java
@@ -1,9 +1,6 @@
 package br.com.dh.clinica.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
