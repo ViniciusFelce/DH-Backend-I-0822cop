ecb4975b645e2302e259ccfd978fa493fb9c673d
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2022-09-13 09:55:54 -0300

    Aula35 - CRUD completo Paciente-Endereço.

--- 35 - Integrador XI/src/main/java/br/com/dh/clinica/controllers/DentistaController.java
+++ 35 - Integrador XI/src/main/java/br/com/dh/clinica/controllers/DentistaController.java
@@ -1,7 +1,7 @@
 package br.com.dh.clinica.controllers;
 
-import br.com.dh.clinica.dtos.PacienteDto;
-import br.com.dh.clinica.services.PacienteService;
+import br.com.dh.clinica.dtos.DentistaDto;
+import br.com.dh.clinica.services.DentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,32 +11,32 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/pacientes")
-public class PacienteController {
+@RequestMapping(value = "/dentistas")
+public class DentistaController {
 
     @Autowired
-    PacienteService service;
+    DentistaService service;
 
     @GetMapping
-    public ResponseEntity<List<PacienteDto>> buscarTodosOsPacientes() {
-        List<PacienteDto> list = service.buscarTodos();
+    public ResponseEntity<List<DentistaDto>> buscarTodosOsDentistas() {
+        List<DentistaDto> list = service.buscarTodos();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<PacienteDto> buscarPacientePorId(@PathVariable Integer id) {
-        PacienteDto dto = service.buscarPorId(id);
+    public ResponseEntity<DentistaDto> buscarDentistaPorId(@PathVariable Integer id) {
+        DentistaDto dto = service.buscarPorId(id);
         return ResponseEntity.ok().body(dto);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> excluirPaciente(@PathVariable Integer id) {
+    public ResponseEntity<Void> excluirDentista(@PathVariable Integer id) {
         service.excluir(id);
         return ResponseEntity.noContent().build(); // Retorna 204
     }
 
     @PostMapping
-    public ResponseEntity<PacienteDto> inserirPaciente(@RequestBody PacienteDto dto) {
+    public ResponseEntity<DentistaDto> inserirDentista(@RequestBody DentistaDto dto) {
         dto = service.inserir(dto); // O dto que retornou agora tem ID
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
@@ -44,7 +44,7 @@ public class PacienteController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<PacienteDto> atualizarPaciente(@PathVariable Integer id,  @RequestBody PacienteDto dto) {
+    public ResponseEntity<DentistaDto> atualizarDentista(@PathVariable Integer id,  @RequestBody DentistaDto dto) {
         dto = service.atualizar(id, dto);
         return ResponseEntity.ok().body(dto);
     }
